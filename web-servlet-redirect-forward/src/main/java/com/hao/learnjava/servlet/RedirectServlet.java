295052207c23786fd3746b828cd6911645baf039
haokw <atwanghao@gmail.com>
2020-12-25 21:59:55 +0800

    web-servlet-redirect-forward

---
+++ web-servlet-redirect-forward/src/main/java/com/hao/learnjava/servlet/RedirectServlet.java
@@ -0,0 +1,18 @@
+package com.hao.learnjava.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hi")
+public class RedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String redirectToUrl = "/hello" + (name == null ? "" : "?name=" + name);
+        resp.sendRedirect(redirectToUrl);
+    }
+}
